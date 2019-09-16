acf053e85cc7a1939437ade71a6aa3f2e70dd4de
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-16 16:52:11 -0600

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header >
+    <h1>Nasa Pic of the Day</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name:"Header"
+}
+</script>
+
+<style scoped>
+  header{
+    background: #333;
+    color:#fff;
+    text-align: center;
+    padding:10px;
+  }
+
+  header a {
+    color:#fff;
+    padding-right: 5px;
+  }
+
+</style>
