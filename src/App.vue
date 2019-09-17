5f1fabdc84fa1168d8f82441b89576bc439729ca
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-16 21:22:35 -0600

    Add components and data to App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
     <Header/>
+    <Planets v-bind:planetData="planetData"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Planets from './components/Planets'
+import fetchPlanetData from './nasaFetch'
+
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Planets
+  }, 
+  data() {
+    return {
+      planetData:[]
+    }
+  },
+  async created() {
+    this.planetData = await fetchPlanetData()
   }
 }
 </script>
@@ -20,10 +33,10 @@ export default {
   margin:0;
   padding:0;
 }
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
 }
 </style>
