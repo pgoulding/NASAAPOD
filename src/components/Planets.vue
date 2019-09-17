d7033192b383f97bdf412743474b249ea966f94d
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-16 21:21:17 -0600

    add planets container

---
+++ src/components/Planets.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <div v-bind:key="planet.id" v-for="planet in planetData">
+      <PlanetCard v-bind:planet="planet"/>
+    </div>
+  </section>
+</template>
+
+<script>
+import PlanetCard from './PlanetCard'
+
+export default {
+  name: "Planets",
+  components:{
+    PlanetCard
+  },
+  props: ["planetData"]
+}
+</script>
+
+<style scoped>
+.container {  
+    display: grid;  
+    grid-gap: 5px;  
+    background: #f4f4f4;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+    /* grid-template-rows: 260px; */
+    padding:5px;
+}
+/* .container > div > img {  
+    width: 100%;  
+    height: 100%;  
+    object-fit: cover;  
+} */
+</style>
