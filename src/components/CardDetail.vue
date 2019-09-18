7dd6b019078ea7e657d8dd463f886617a1d4b60d
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-18 00:55:35 -0600

    Insert Place holder data for card detail

---
+++ src/components/CardDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Planet Title</h2>
+    <img src="" alt="image description"/>
+    <p>More description under photo</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'CardDetail'
+}
+</script>
