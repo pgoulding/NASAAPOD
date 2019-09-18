5ccd6dc59e17576d59a906ebfda477d5bea822bd
Patrick Goulding <patrickgoulding@gmail.com>
2019-09-18 02:31:03 -0600

    Add details and initial styling to Card Detail

--- src/components/CardDetail.vue
+++ src/components/CardDetail.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <h2>Planet Title</h2>
-    <img src="" alt="image description"/>
-    <p>More description under photo</p>
+  <div class="detail-card">
+    <h2>{{card.title}}</h2>
+    <p>{{card.date}}</p>
+    <img :alt="card.explanation" v-if="card.media_type === 'image'" class="card-image" :src="card.url"/>
+    <iframe v-else type="text/html" width="100%" height="240px" :src="card.url"></iframe>
+    <p>{{card.explanation}}</p>
   </div>
 </template>
 
 <script>
+import { planetDataOnDate } from '../nasaFetch'
+
 export default {
-  name:'CardDetail'
+  name:'CardDetail',
+  data () {
+    return  {
+      date:this.$route.params,
+      card:{}
+    }
+  },
+  async created () {
+    this.card = await planetDataOnDate(this.date.id)
+  }
 }
 </script>
+
+<style scoped>
+  .detail-card {
+    width:90%;
+    margin: 30px auto;
+  }
+</style>
